2ee6d0500bea05dd21e7499bf10ab2b81ebcee1a
lama saad <lamasaad65@gmail.com>
2023-11-25 19:55:22 +0300

    lh

--- src/assets/components/products/Products.jsx
+++ src/assets/components/products/Products.jsx
@@ -4,13 +4,17 @@ export default function Products() {
 
     let [products,setProducts] = useState([]);
 
-    const getProducts = async ()=>{
-        let response = await fetch("https://fakestoreapi.com/products");
-        let data = await response.json();
-        // console.log(data);
-        setProducts(data);
+    // const getProducts = async ()=>{
+    //     let response = await fetch("https://fakestoreapi.com/products");
+    //     let data = await response.json();
+    //     // console.log(data);
+    //     setProducts(data);
+    // }
+    const getProducts = async () => {
+        const data = await fetch('https://fakestoreapi.com/products')
+            .then(res => res.json());
+            setProducts(data);
     }
-
     useEffect( ()=>{
         getProducts();
     }, [])
